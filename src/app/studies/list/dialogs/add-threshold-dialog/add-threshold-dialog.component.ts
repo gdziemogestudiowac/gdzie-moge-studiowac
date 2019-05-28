82a7d65e9a07ec54cc2e3b4a28d860e8fbe871e2
spartanPAGE <spartanpageagain@gmail.com>
2019-05-28 13:22:06 +0200

    add buttons with exam ids

--- src/app/studies/list/dialogs/add-threshold-dialog/add-threshold-dialog.component.ts
+++ src/app/studies/list/dialogs/add-threshold-dialog/add-threshold-dialog.component.ts
@@ -13,9 +13,45 @@ export class AddThresholdDialogComponent {
     @Inject(MAT_DIALOG_DATA) public data: StudyField
   ) {}
 
+  exams = {
+    "ustny polski": "UP",
+    "ustny nowożytni": "UN",
+
+    "pdst polski": "PP",
+    "pdst nowożytni": "PN",
+
+    "pdst mat": "PM",
+
+    "roz. polski": "RP",
+    "roz. mat": "RMAT",
+    "roz. łac. i kultura": "RLACIK",
+    "roz. WOS": "RWOS",
+    "roz. INF": "RINF",
+    "roz. BIOL": "RBIOL",
+    "roz. FIL": "RFIL",
+    "roz. HIS": "RHIS",
+    "roz. h. sztuki": "RHS",
+    "roz. chem": "RCHEM",
+    "roz. geo": "RGEO",
+    "roz. fiz": "RFIZ",
+    "roz. h. muzyki": "RHM",
+    "roz. angielski": "RA",
+    "roz. niemiecki": "RN",
+    "roz. rosyjski": "RR",
+    "roz. francuski": "RFR",
+    "roz. ukraiński": "RUK",
+    "roz. białoruski": "RBIA",
+    "roz. hiszpański": "RHISZP",
+    "roz. włoski": "RWLOS"
+  };
+
   threshold = {
     year: 2018,
     value: 0,
     formula: ""
   };
+
+  addToFormula(value) {
+    this.threshold.formula += value;
+  }
 }
